766aa6fa0535ddeffba86d5856bf65199866eb3a
Kauã Biasebetti <kauamengel17@gmail.com>
2025-03-26 21:54:32 -0300

    Implementando estrutura básica do jogo e entrada de dados do usuário

--- JogoDaForca.ConsoleApp/Program.cs
+++ JogoDaForca.ConsoleApp/Program.cs
@@ -2,9 +2,24 @@
 {
     internal class Program
     {
+        // Versão 01: Estrutura básica e entrada do usuário
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("-----------------");
+                Console.WriteLine("| Jogo da Forca |");
+                Console.WriteLine("-----------------");
+
+                Console.Write("Digite uma letra: ");
+                char chute = Console.ReadLine()[0];
+
+                Console.WriteLine($"Você digitou: {chute}");
+                Console.ReadLine();
+            } while (true);
+
+            Console.ReadLine();
         }
     }
 }
